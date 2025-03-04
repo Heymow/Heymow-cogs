19fbec7f154695cdbc3cbdb2bdd1949744e03f6e
Khaled CHEHAB <lynckit@hotmail.com>
2025-03-04 10:39:49 +0100

    Fix LinkChecker class name in initialization for consistency

--- linkchecker/__init__.py
+++ linkchecker/__init__.py
@@ -1,11 +1,11 @@
 import json
 from pathlib import Path
 
-from .linkchecker import Linkchecker
+from .linkchecker import LinkChecker
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 async def setup(bot):
-    n = Linkchecker(bot)
-    await bot.add_cog(n)
+    cog = LinkChecker(bot)
+    await bot.add_cog(cog)
