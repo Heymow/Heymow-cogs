cc074a5cee0d97fcfec5662affd6ae1d29b7d8c2
Khaled CHEHAB <lynckit@hotmail.com>
2025-10-31 17:47:35 +0100

    Remove commented code for default host/port instantiation in StreamRoles API setup

--- streamroles_api/__init__.py
+++ streamroles_api/__init__.py
@@ -9,5 +9,4 @@ from .streamroles_api_cog import StreamRolesAPI  # noqa: E402,F401
 
 async def setup(bot):
     """Red cog loader entrypoint."""
-    # instantiate with default host/port which are read from env in the cog
     await bot.add_cog(StreamRolesAPI(bot))
